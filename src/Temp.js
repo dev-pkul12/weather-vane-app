45c494f6df20516c16a8b06f4dbac6f5aedddddb
Prasanna Kulkarni <104126691+prasannakulkarni12@users.noreply.github.com>
2023-04-18 13:39:54 +0530

    Update Temp.js

--- src/Temp.js
+++ src/Temp.js
@@ -8,8 +8,7 @@ const Temp = () => {
 
     const getWeatherInfo = async () => {
         try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=d97b0803288e5ebe6e577cad7c73f838
-            `;
+            let url = `https://api.openweathermap.org/`;
 
             let res = await fetch(url);
             let data = await res.json();
